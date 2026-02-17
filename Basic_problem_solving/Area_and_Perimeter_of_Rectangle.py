b2ca0ee439db12d2c474aaa8b76d33daed878061
pamudhurthichaitanya-45 <pamudhurthichaitanya45@gmail.com>
2026-02-17 23:00:41 +0530

    Add Area and Perimeter calculation for rectangle

---
+++ Basic_problem_solving/Area_and_Perimeter_of_Rectangle.py
@@ -0,0 +1,6 @@
+length=float(input("Enter the area of the rectangle:"))
+width=float(input("Enter the perimeter of the rectangle:"))
+area = length * width
+perimeter = 2 * (length + width)
+print("Area:",area)
+print("Perimeter:",perimeter)
